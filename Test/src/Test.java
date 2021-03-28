9999dd31b3683fd30d5973106d59cb41de274dab
Ibrahim <Ibrahim@DESKTOP-V7L6GKP>
2021-03-28 21:06:32 +0300

    Test

---
+++ Test/src/Test.java
@@ -0,0 +1,22 @@
+
+ class Test {
+int i;
+//String i; --- null
+ }
+ class main{
+	 
+ 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Test t; ---- compile Error
+		
+		//default constructor
+Test t= new Test();
+
+System.out.println(t.i);
+
+//search for: Default values for primitive data types 
+	
+	}
+}
