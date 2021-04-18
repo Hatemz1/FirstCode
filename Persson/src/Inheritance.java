1aa8728a9f6018d7290a39fe56db150fc1aa9112
Ibrahim <Ibrahim@DESKTOP-V7L6GKP>
2021-04-18 23:05:02 +0300

    Inheritance

---
+++ Persson/src/Inheritance.java
@@ -0,0 +1,10 @@
+
+public class Inheritance {
+
+    public static void main(String[] args) {
+        Person p =new Person("Ahmed",22);
+        Teacher t =new Teacher("Ali",50,55000);
+        System.out.println(p.name);
+        System.out.println(t.name);
+}
+ }
---
+++ Persson/src/Person.java
@@ -0,0 +1,19 @@
+
+public class Person {
+
+    public String name;
+    public int age;
+    Person(String name,int age){
+        this.name=name;
+        this.age=age;
+    }
+}
+class Teacher extends Person{
+    public int salary;
+    Teacher(String name,int age,int salary){
+        super(name,age);
+        this.salary=salary;
+    }
+    
+}
+
